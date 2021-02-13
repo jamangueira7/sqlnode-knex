16e4e26bc907bf05128cfaebfb19edcad69d212a
João Mangueira <jamjs7@gmail.com>
2021-02-13 19:35:41 -0300

    ad configurações knex

---
+++ knexfile.js
@@ -0,0 +1,13 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      database: "knex_test",
+      user: "postgres",
+      password: "docker",
+    }
+  }
+};
