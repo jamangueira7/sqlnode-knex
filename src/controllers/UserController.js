f35fe31fab18a3d11572529fb54d2ed3092c30dd
João Mangueira <jamjs7@gmail.com>
2021-02-14 14:13:32 -0300

    list obedecendo a regra de softDelete

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -3,7 +3,7 @@ const knex = require('../database');
 module.exports = {
     async index (req, res, next) {
         try {
-            const users = await knex('users');
+            const users = await knex('users').where('deleted_at', null);
 
             return res.json(users);
         } catch (err) {
