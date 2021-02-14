7f7aba9e04b3a0ff1ba6cca12e64fef557fb5edc
João Mangueira <jamjs7@gmail.com>
2021-02-13 21:11:53 -0300

    usando join

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -7,7 +7,10 @@ module.exports = {
             const query = knex('projects');
 
             if(userId) {
-                query.where({ user_id: userId });
+                query
+                    .where({ user_id: userId })
+                    .join('users', 'users.id', 'projects.user_id')
+                    .select('projects.*', 'users.username');
             }
 
             const projects = await query;
