2031cc6dee7ff7e79faf7a030e9ffddb2033189b
João Mangueira <jamjs7@gmail.com>
2021-02-13 21:00:08 -0300

    migrate projects

--- src/database/migrations/20210213193841_create_table_users.js
+++ src/database/migrations/20210213193841_create_table_users.js
@@ -1,9 +1,9 @@
 
 exports.up = (knex) => knex.schema.createTable('users', (table) => {
-      table.increments('id')
-      table.text('username').unique().notNullable()
-      table.timestamp('created_at').defaultTo(knex.fn.now())
-      table.timestamp('updated_at').defaultTo(knex.fn.now())
+      table.increments('id');
+      table.text('username').unique().notNullable();
+      table.timestamp('created_at').defaultTo(knex.fn.now());
+      table.timestamp('updated_at').defaultTo(knex.fn.now());
 });
 
 
